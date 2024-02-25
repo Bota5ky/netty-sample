03c84d3890d2081776e5cc007af4d75c24e4bc66
Bota5ky <zjy2516@126.com>
2024-02-25 08:59:45 +0800

    test allocate, rewind, mark and reset

--- src/test/java/ByteBufferTest.java
+++ src/test/java/ByteBufferTest.java
@@ -6,6 +6,7 @@ import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
 import static io.bota5ky.ByteBufferUtil.debugAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author Bota5ky
@@ -13,7 +14,7 @@ import static io.bota5ky.ByteBufferUtil.debugAll;
  */
 public class ByteBufferTest {
     @Test
-    void should_read_limit_data_from_file_when_use_byte_buffer_given_file_channel() {
+    void read_limit_data() {
         // 获得FileChannel的两种方式
         // 1.输入输出流 2.RandomAccessFile
         try (FileInputStream fileInputStream = new FileInputStream("src/test/resources/data.txt")) {
@@ -31,7 +32,7 @@ public class ByteBufferTest {
     }
 
     @Test
-    void should_read_all_data_from_file_when_use_byte_buffer_given_file_channel() {
+    void read_all_data() {
         try (FileInputStream fileInputStream = new FileInputStream("src/test/resources/data.txt")) {
             FileChannel channel = fileInputStream.getChannel();
             ByteBuffer buffer = ByteBuffer.allocate(10);
@@ -50,7 +51,7 @@ public class ByteBufferTest {
     }
 
     @Test
-    void should_flip_compact_when_use_byte_buffer_given_file_channel() {
+    void test_flip_and_compact() {
         ByteBuffer buffer = ByteBuffer.allocate(10);
         buffer.put((byte) 0x61); // a
         debugAll(buffer);
@@ -64,6 +65,42 @@ public class ByteBufferTest {
         debugAll(buffer);
         // 注释第一次打印，可以明显看到数据前移，并保留了原来的数据
         buffer.compact();
+        debugAll(buffer);
+    }
+
+    @Test
+    void show_allocate_and_allocate_direct_difference() {
+        assertEquals("class java.nio.HeapByteBuffer", ByteBuffer.allocate(10).getClass().toString());
+        assertEquals("class java.nio.DirectByteBuffer", ByteBuffer.allocateDirect(10).getClass().toString());
+        // class java.nio.HeapByteBuffer - java堆内存，读写效率低，受到GC的影响
+        // class java.nio.DirectByteBuffer - 直接内存操作系统控制，读写效率高（少一次拷贝），不会受GC影响，但分配效率低，容易造成内存泄露
+    }
+
+    @Test
+    void test_rewind_mark_and_reset() {
+        ByteBuffer buffer = ByteBuffer.allocate(10);
+        buffer.put(new byte[]{'a', 'b', 'c', 'd'});
+        buffer.flip();
+
+        buffer.get(new byte[4]);
+        debugAll(buffer);
+        buffer.rewind();
+        buffer.get(new byte[4]);
+        debugAll(buffer);
+        buffer.rewind();
+        // mark 记录一个position位置
+        // reset 将position重置到mark位置
+        System.out.println((char) buffer.get());
+        System.out.println((char) buffer.get());
+        buffer.mark(); // 加标记，索引2的位置
+        System.out.println((char) buffer.get());
+        System.out.println((char) buffer.get());
+        buffer.reset(); // 将position重置到索引2
+        System.out.println((char) buffer.get());
+        System.out.println((char) buffer.get());
+
+        // get(i)不改变position
+        System.out.println((char) buffer.get(3));
         debugAll(buffer);
     }
 }
